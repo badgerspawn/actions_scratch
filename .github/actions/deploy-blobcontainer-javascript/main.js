0a28eefc212f9f9824a9dc709bc75e0f09912d2f
SJB <simon@outofthewest.co.uk>
2025-02-20 14:36:08 +0000

    Commit with TODOs for js action

--- .github/actions/deploy-blobcontainer-javascript/main.js
+++ .github/actions/deploy-blobcontainer-javascript/main.js
@@ -3,6 +3,8 @@ const github = require('@actions/github');
 const exec = require('@actions/exec');
 
 function run() {
+  // TODO: exercise Get the inputs for Azure blob
+  // upload to the blob
   core.info('Hello World from a custom javascript action!');
 }
 
